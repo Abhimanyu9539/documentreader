461b81f76cd20ae5ac26ba80a5f0aca1e6b0135a
Abhimanyu <iabhimanyu31@gmail.com>
2025-10-29 20:00:00 +0530

    Add Chat Ingestion

--- src/document_ingestion/data_ingestion.py
+++ src/document_ingestion/data_ingestion.py
@@ -20,8 +20,8 @@ from utils.model_loader import ModelLoader
 from custom_logging.custom_logger import CustomLogger
 from exception.custom_exception import DocumentPortalException
 
-#from utils.file_io import session_id, save_uploaded_file
-#from utils.document_ops import load_documents, concat_for_analysis, concact_for_comparison
+from utils.file_io import session_id, save_uploaded_files
+from utils.document_ops import load_documents, concat_for_analysis, concact_for_comparison
 
 SUPPORTED_EXTENSIONS = {'.pdf', '.docx', '.txt'}
 
@@ -83,16 +83,23 @@ class FAISSManager:
         
         return len(new_docs)
 
-    def load_or_create(self):
+    def load_or_create(self,texts:Optional[List[str]]=None, metadatas: Optional[List[dict]] = None):
+        ## if we running first time then it will not go in this block
         if self._exists():
             self.vs = FAISS.load_local(
                 str(self.index_dir),
-                embeddings= self.embedder,
-                allow_dangerous_deserialization=True
+                embeddings=self.emb,
+                allow_dangerous_deserialization=True,
             )
-
             return self.vs
-
+        
+        
+        if not texts:
+            raise DocumentPortalException("No existing FAISS index and no data to create one", sys)
+        
+        self.vs = FAISS.from_texts(texts=texts, embedding=self.emb, metadatas=metadatas or [])
+        self.vs.save_local(str(self.index_dir))
+        return self.vs
 
 class DocHandler:
     """"pdf SAVE + READ handler for page analysis"""
@@ -124,8 +131,24 @@ class DocHandler:
         except Exception as e:
             self.logger.error("Failed to save pdf", error = str(e), session_id = self.session_id)
             raise DocumentPortalException(f"Failed to save pdf: {str(e)}", e ) from e
-    def load_pdf(self):
-        pass
+    
+    
+    def read_pdf(self, pdf_path:str) -> str:
+        try:
+            text_chunks = []
+            with fitz.open(pdf_path) as doc:
+                for page_num in range(doc.page_count):
+                    page = doc.load_page(page_num)
+                    text_chunks.append(f"\n=== Page {page_num + 1} ---\n {page.get_text()}")
+
+                text = "\n".join(text_chunks)
+                self.logger.info("PDF read successfully", pdf_path=pdf_path, session_id = self.session_id, pages=len(text_chunks))
+                return text
+        except Exception as e:
+            self.logger.error("Failed to read pdf", error=str(e), pdf_path=pdf_path, session_id=self.session_id)
+            raise DocumentPortalException(f"Could not process the pdf:{pdf_path}", e ) from e
+        
+        
 
 class DocumentComparator:
     def __init__(self):
@@ -146,14 +169,77 @@ class DocumentComparator:
 
 
 class ChatIngestor:
-    def __init__(self):
-        pass
+    def __init__( self,
+        temp_base: str = "data",
+        faiss_base: str = "faiss_index",
+        use_session_dirs: bool = True,
+        session_id: Optional[str] = None,
+    ):
+        try:
+            self.model_loader = ModelLoader()
+            self.logger = CustomLogger.get_logger(__name__)
+            self.use_session = use_session_dirs
+            self.session_id = session_id #or generate_session_id()
+            
+            self.temp_base = Path(temp_base); self.temp_base.mkdir(parents=True, exist_ok=True)
+            self.faiss_base = Path(faiss_base); self.faiss_base.mkdir(parents=True, exist_ok=True)
+            
+            self.temp_dir = self._resolve_dir(self.temp_base)
+            self.faiss_dir = self._resolve_dir(self.faiss_base)
+
+            self.logger.info("ChatIngestor initialized",
+                      session_id=self.session_id,
+                      temp_dir=str(self.temp_dir),
+                      faiss_dir=str(self.faiss_dir),
+                      sessionized=self.use_session)
+        except Exception as e:
+            self.logger.error("Failed to initialize ChatIngestor", error=str(e))
+            raise DocumentPortalException("Initialization error in ChatIngestor", e) from e
+            
+        
+    def _resolve_dir(self, base: Path):
+        if self.use_session:
+            d = base / self.session_id # e.g. "faiss_index/abc123"
+            d.mkdir(parents=True, exist_ok=True) # creates dir if not exists
+            return d
+        return base # fallback: "faiss_index/"
+        
+    def _split(self, docs: List[Document], chunk_size=1000, chunk_overlap=200) -> List[Document]:
+        splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+        chunks = splitter.split_documents(docs)
+        self.logger.info("Documents split", chunks=len(chunks), chunk_size=chunk_size, overlap=chunk_overlap)
+        return chunks
     
-    def _resolve_dir(self):
-        pass
-
-    def _split(self):
-        pass
-
-    def _build_retriever(self):
-        pass
+    def built_retriver( self,
+        uploaded_files: Iterable,
+        *,
+        chunk_size: int = 1000,
+        chunk_overlap: int = 200,
+        k: int = 5,):
+        try:
+            paths = save_uploaded_files(uploaded_files, self.temp_dir)
+            docs = load_documents(paths)
+            if not docs:
+                raise ValueError("No valid documents loaded")
+            
+            chunks = self._split(docs, chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+            
+            ## FAISS manager very very important class for the docchat
+            fm = FAISSManager(self.faiss_dir, self.model_loader)
+            
+            texts = [c.page_content for c in chunks]
+            metas = [c.metadata for c in chunks]
+            
+            try:
+                vs = fm.load_or_create(texts=texts, metadatas=metas)
+            except Exception:
+                vs = fm.load_or_create(texts=texts, metadatas=metas)
+                
+            added = fm.add_documents(chunks)
+            self.logger.info("FAISS index updated", added=added, index=str(self.faiss_dir))
+            
+            return vs.as_retriever(search_type="similarity", search_kwargs={"k": k})
+            
+        except Exception as e:
+            self.logger.error("Failed to build retriever", error=str(e))
+            raise DocumentPortalException("Failed to build retriever", e) from e
